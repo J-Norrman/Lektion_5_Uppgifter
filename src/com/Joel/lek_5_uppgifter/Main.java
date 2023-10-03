675ece8415d954175553c75c2fbb3528a773dbd3
Joel Norrman <joel-norrman@hotmail.com>
2023-10-03 13:33:47 +0200

    uppgift 6

--- src/com/Joel/lek_5_uppgifter/Main.java
+++ src/com/Joel/lek_5_uppgifter/Main.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int number;
+        String string;
 
         System.out.println("What is your name?");
         String playerName = scanner.nextLine();
@@ -15,6 +17,28 @@ public class Main {
         for (int i = 0; i < givenNumber; i++) {
             System.out.println("looping...");
         }
+
+        do {
+            System.out.println("enter a valid number");
+            while (!scanner.hasNextInt()) {
+                System.out.println("PLEASE enter a valid number");
+                scanner.next();
+            }
+            number = scanner.nextInt();
+            System.out.println("Thank you for " + number);
+        }while (number <= 0);
+
+//        do {
+//            System.out.println("enter a valid String");
+//            while (!scanner.hasNextLine()) {
+//                System.out.println("PLEASE enter a valid String");
+//                scanner.next();
+//            }
+//            string = scanner.nextLine();
+//        }while (scanner.hasNextLine());
+
+        // I tried
+
         do {
             System.out.println("Type 1 to throw. Type stop to exit");
             String svar = scanner.next();
